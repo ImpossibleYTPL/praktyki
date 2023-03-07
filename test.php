ebb0aeb6c88ee3d14b7ae9f317a64abba826bc9d
ImpossibleYTPL <77750579+ImpossibleYTPL@users.noreply.github.com>
2023-03-07 11:20:50 +0100

    ok

--- test.php
+++ test.php
@@ -4,60 +4,76 @@
 
 //kandydat
 
-$firstname;
-$secondname;
-$surname;
-$bornDate;
-$bornLocation;
-$PESEL;
-$tel;
-$mail;
+$firstname = $_POST['Imie'];
+$secondname = $_POST['drugie_imie'];
+$surname = $_POST['Nazwisko'];
+$bornDate = $_POST['Data_ur'];
+$bornLocation = $_POST['miejsce_ur'];
+$PESEL = $_POST['PESEL'];
+$tel = $_POST['nr_tel'];
+$mail = $_POST['email'];
 
 //adres kandydata
 
-$KAcity;
-$KAstreet;
-$KAGMINA;
-$KApostcode;
-$KApost;
+$KAcity = $_POST['miejsce_zam'];
+$KAstreet = $_POST['ulica'];
+$KAGMINA = $_POST['gmina'];
+$KApostcode = $_POST['kod_pocztowy'];
+$KApost = $_POST['poczta'];
 
 //adres zameldowania
 
-$MAcity;
-$MAstreet;
-$MAGMINA;
-$MApostcode;
-$MApost;
+$MAcity = (!$_POST['zamche']) ? $_POST['miejsce_zamel'] : $KAcity;
+$MAstreet = (!$_POST['zamche']) ? $_POST['ulica_zamel'] : $KAstreet;
+$MAGMINA = (!$_POST['zamche']) ? $_POST['gmina_zamel'] : $KAGMINA;
+$MApostcode = (!$_POST['zamche']) ? $_POST['kod_pocztowy_zamel'] : $KApostcode;
+$MApost = (!$_POST['zamche']) ? $_POST['poczta'] : $KApost;
 
 //opiekunowie
 //matka
 
-$MDname;
-$MDsurname;
-$MDcity;
-$MDstreet;
-$MDGMINA;
-$MDpostcode;
-$MDpost;
+$MDname = $_POST['Imie_matki'];
+$MDsurname = $_POST['Nazwisko_matki'];
+$MDcity = (!$_POST['MATche']) ? $_POST['miejsceMAT'] : $KAcity;
+$MDstreet = (!$_POST['MATche']) ? $_POST['ulicaMAT'] : $KAstreet;
+$MDGMINA = (!$_POST['MATche']) ? $_POST['gminaMAT'] : $KAGMINA;
+$MDpostcode = (!$_POST['MATche']) ? $_POST['kod_pocztowyMAT'] : $KApostcode;
+$MDpost = (!$_POST['MATche']) ? $_POST['pocztaMAT'] : $KApost;
+$MDtel = $_POST['nr_tel_matki'];
+$MDmail = $_POST['email_matki'];
 
 //ojciec
 
-$FDname;
-$FDsurname;
-$FDcity;
-$FDstreet;
-$FDGMINA;
-$FDpostcode;
-$FDpost;
+$FDname = $_POST['Imie_ojca'];
+$FDsurname = $_POST['Nazwisko_ojca'];
+$FDcity = (!$_POST['OJCAche']) ? $_POST['miejscOJCA'] : $KAcity;
+$FDstreet = (!$_POST['OJCAche']) ? $_POST['ulicaOJCA'] : $KAstreet;
+$FDGMINA = (!$_POST['OJCAche']) ? $_POST['gminaOJCA'] : $KAGMINA;
+$FDpostcode = (!$_POST['OJCAche']) ? $_POST['kod_pocztowyOJCA'] : $KApostcode;
+$FDpost = (!$_POST['OJCAche']) ? $_POST['pocztaOJCA'] : $KApost;
+$FDtel = $_POST['nr_tel_ojca'];
+$FDmail = $_POST['email_ojca'];
 
 //opiekun
 
-$GDname;
-$GDsurname;
-$GDcity;
-$GDstreet;
-$GDGMINA;
-$GDpostcode;
-$GDpost;
+$GDname = $_POST['Imie_op'];
+$GDsurname = $_POST['Nazwisko_op'];
+$GDcity = $_POST['miejsce_zGD'];
+$GDstreet = $_POST['ulicaGD'];
+$GDGMINA = $_POST['gmidaGD'];
+$GDpostcode = $_POST['kod_pocztowyGD'];
+$GDpost = $_POST['pocztaGD'];
+$GDtel = $_POST['nr_tel_op'];
+$GDmail = $_POST['email_op'];
+
+//wniosek
+$date = $_POST['data'];
+$time = $_POST['godz'];
+
+//Profile:
+
+$profil1 = $_POST['profil'];
+$profil2 = $_POST['profil2'];
+$profil3 = $_POST['profil3'];
 
 ?>
