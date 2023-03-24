090d0936fa6ed1788e6f1d8e73511958755d3004
ImpossibleYTPL <77750579+ImpossibleYTPL@users.noreply.github.com>
2023-03-24 09:00:55 +0100

    Update test.php

--- test.php
+++ test.php
@@ -4,17 +4,27 @@ require_once "DataBase.php";
 
 $link = new mysqli($servername, $username, $password, $dbname);
 
+if($link->errno != 0) {
+    die("Błąd połączenia ".$link->errno);
+}
+
 $safePost = filter_input(INPUT_POST, 'Nazwisko', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
 $data = array();
 foreach ($_POST as $key => $value) {
-    $safePost = str_replace([';','-'],'',$safePost);
-    $safePost = trim($safePost);
-    $safePost = strtolower($safePost);
-    $safePost = ucfirst($safePost);
+    $value = str_replace([';','-'],'',$value);
+    $value = trim($value);
+    $value = strtolower($value);
+    $value = ucfirst($value);
     $data[$key] = $value;
 }
 
-var_dump($data);
+$pdo = $link->prepare("SELECT * FROM `kandydat` WHERE `Pesel` = ?");
+$pdo->bind_param("s", "$data[Pesel]");
+$pdo->execute();
+
+if($pdo->num_rows() >= 1) {
+    die("Taki pesel znajduje się już w bazie danych");
+}
 
 ?>
